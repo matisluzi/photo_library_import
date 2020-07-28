6dcbb52b28ea8ff40000f044adc051ff5c424411
Matis Luzi <matisluzi@gmail.com>
2020-07-28 14:07:02 +0200

    Initial Commit

---
+++ Photo Library Import/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Photo Library Import
+//
+//  Created by Matis Luzi on 7/28/20.
+//  Copyright © 2020 Matis Luzi. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Photo Library ImportTests/Photo_Library_ImportTests.swift
@@ -0,0 +1,34 @@
+//
+//  Photo_Library_ImportTests.swift
+//  Photo Library ImportTests
+//
+//  Created by Matis Luzi on 7/28/20.
+//  Copyright © 2020 Matis Luzi. All rights reserved.
+//
+
+import XCTest
+@testable import Photo_Library_Import
+
+class Photo_Library_ImportTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
